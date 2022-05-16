8770a0e018afc7f8ca7d09147bd704d2f4c389fb
채수연 <sooyeonchae@gmail.com>
2022-05-16 16:57:29 +0900

    intent(2) 데이터를 들고 화면 이동하기 구조 작성

--- app/src/main/java/net/soozip/intent_20220516/MainActivity.kt
+++ app/src/main/java/net/soozip/intent_20220516/MainActivity.kt
@@ -1,11 +1,31 @@
 package net.soozip.intent_20220516
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnMoveToOther.setOnClickListener {
+//            val myIntent = Intent(this, OtherActivity::class.java)
+//            startActivity(myIntent)
+
+            //현재 화면 종료 > 이전 화면이 다시 나타남
+            finish()
+        }
+
+        btnSendMessage.setOnClickListener{
+
+            //1. 입력한 내용을 변수에 저장
+
+            //2. 메시지 보여주는 화면으로(1.의 변수를 들고) 이동.
+        }
+
     }
+
 }
--- app/src/main/java/net/soozip/intent_20220516/OtherActivity.kt
+++ app/src/main/java/net/soozip/intent_20220516/OtherActivity.kt
@@ -2,10 +2,16 @@ package net.soozip.intent_20220516
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnMoveToMain.setOnClickListener{
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
